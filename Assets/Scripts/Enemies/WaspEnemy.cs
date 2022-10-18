c4806f83ec5a0f48d7ff4bd46b54a6e386f230cb
Tom Scott <tom.scott45456@gmail.com>
2022-10-18 23:58:49 +0100

    Wasp Enemy Attack
    If the player gets in range of the wasp, it beelines (should that be wasplines?) towards the player. Upon collision, it stays in place for a short while before return back to its flying height and then cooling down its attack

--- Assets/Scripts/Enemies/WaspEnemy.cs
+++ Assets/Scripts/Enemies/WaspEnemy.cs
@@ -12,6 +12,11 @@ public class WaspEnemy : EnemyBase
 
     [SerializeField] private float m_diveBombSpeed;
 
+    [SerializeField] private float m_attackCooldownDuration;
+    private float m_attackCooldownTimer = 0f;
+
+    private bool m_hasRisen = false;
+
     private Vector2 m_origin;
 
     private float m_sinCounter;
@@ -20,9 +25,8 @@ public class WaspEnemy : EnemyBase
     {
         eFlying,
         eDiveBomb,
-        eRiseUp,
-        eAttack,
-        eDead
+        eCoolDown,
+        eAttack
     }
 
     private eState m_state;
@@ -48,7 +52,6 @@ public class WaspEnemy : EnemyBase
                     if (distanceToPlayer <= m_minDistanceToPlayer * m_minDistanceToPlayer)
                     {
                         m_state = eState.eDiveBomb;
-                        DiveBomb();
                     }
                     else
                     {
@@ -59,8 +62,8 @@ public class WaspEnemy : EnemyBase
             case eState.eDiveBomb:
                 DiveBomb();
                 break;
-            case eState.eRiseUp:
-                RiseUp();
+            case eState.eCoolDown:
+                CoolDown();
                 break;
             default:
                 DebugLog($"UNHANDLED CASE {m_state}");
@@ -86,7 +89,7 @@ public class WaspEnemy : EnemyBase
         // TODO: Attack the player with the stinger
 
         // Stop from hitting the ground
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.3f);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
 
 #if UNITY_EDITOR
         Debug.DrawRay(transform.position, Vector3.down, Color.cyan);
@@ -94,32 +97,76 @@ public class WaspEnemy : EnemyBase
 
         if (hit.collider != null)
         {
-            // Move back up to the flying Height
-            m_state = eState.eRiseUp;
+            if (hit.collider.gameObject.CompareTag(StringConstants.PLAYER_TAG))
+            {
+                m_state = eState.eAttack;
+                Attack();
+            }
+            else if(hit.collider.gameObject.CompareTag(StringConstants.GROUND_TAG))
+            {
+                // Move back up to the flying Height if we don't hit the player
+                m_state = eState.eCoolDown;
+            }
         }
     }
 
-    private void RiseUp()
+    private void CoolDown()
     {
-        // Fly diagonally upwards to get to the initial height
-        Vector2 toOrigin = new Vector2(
-            m_origin.x - transform.position.x,
-            m_origin.y - transform.position.y
-        );
+        m_attackCooldownTimer += Time.deltaTime;
 
-        toOrigin.Normalize();
-
-        m_rigidbody.velocity = toOrigin * m_speed;
-
-        if (Vector2.Distance(transform.position, m_origin) <= 0.5)
+        if (m_attackCooldownTimer < m_attackCooldownDuration)
         {
+            if (!m_hasRisen)
+            {
+                if (m_origin.y - transform.position.y < 0)
+                {
+                    // Fly diagonally to get to the initial height
+                    m_rigidbody.velocity = Vector2.up * m_speed;
+
+                    transform.position = new Vector3(
+                        transform.position.x,
+                        m_origin.y,
+                        transform.position.y
+                    );
+                }
+                else
+                {
+                    m_hasRisen = true;
+                    DebugLog("REACHED THE HIGHEST POINT!");
+                }
+            }
+            else
+            {
+                Move();
+            }
+
+            DebugLog("COOLING DOWN", BetterDebugging.eDebugLevel.Message);
+        }
+        else
+        {
+            DebugLog("COOLED DOWN", BetterDebugging.eDebugLevel.Message);
+            m_attackCooldownTimer = 0f;
             m_state = eState.eFlying;
         }
     }
 
     protected override void Attack()
     {
+        m_rigidbody.velocity = Vector2.zero;
+        // TODO: Play sting animation
+        // TODO: Play Sound Effect
+        // TODO: Damage the Player
 
+        // TODO: REPLACE WITH PROPER ATTACK STUFF... KEEPING IN PLACE FOR NOW
+        StartCoroutine("AttackPlayer");
+    }
+
+    private IEnumerator AttackPlayer()
+    {
+        DebugLog("DAMAGING THE PLAYER!", BetterDebugging.eDebugLevel.Message);
+        yield return new WaitForSeconds(1.0f);
+        m_state = eState.eCoolDown;
+        DebugLog("FINISHED ATTACKING, RISING BACK UP", BetterDebugging.eDebugLevel.Message);
     }
 
     protected override void Move()
@@ -148,6 +195,11 @@ public class WaspEnemy : EnemyBase
         if (other.gameObject.CompareTag(StringConstants.WALL_TAG))
         {
             TurnAround();
+        }
+        else if (m_state == eState.eDiveBomb && other.gameObject.CompareTag(StringConstants.PLAYER_TAG))
+        {
+            m_state = eState.eAttack;
+            Attack();
         }
     }
 
