7b580d879072fdaeb74c8b3c0baf4b09fd4681fe
LeonHirst <71595884+LeonHirst@users.noreply.github.com>
2022-11-03 12:58:12 +0000

    UI: Transitions
    Added a Level Loader Prefab, which fades in between scenes

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,15 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : Singleton<UIManager>
 {
+    public Animator m_Transition;
 
     public void QuitGame()
     {
         
         Application.Quit();
   
+    }
+
+
+    //Level Loader Code from a brackeys tutorial - https://www.youtube.com/watch?v=CE9VOZivb3I
+    
+    public void LoadNextLevel(string sceneName)
+    {
+        StartCoroutine(LoadLevel(sceneName));
+    }
+
+    
+
+    IEnumerator LoadLevel(string sceneName)
+    {
+        m_Transition.SetTrigger("Fade");
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(sceneName);
     }
 
 
