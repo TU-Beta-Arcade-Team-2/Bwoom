48704579a1cab5e1b6a5907281624204643206ea
Tom Scott <tom.scott45456@gmail.com>
2022-11-09 13:44:00 +0000

    Building tileset from SpriteAtlas
    Using a packed SpriteAtlas, a dictionary of gameobjects is built up ready to be placed in level

--- Assets/Scripts/Level/LevelLoading/LoadLevelFromXML.cs
+++ Assets/Scripts/Level/LevelLoading/LoadLevelFromXML.cs
@@ -1,20 +1,23 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Xml;
-using System;
+using UnityEngine.U2D;
 
 public class LoadLevelFromXML : MonoBehaviour
 {
     public TextAsset LevelXML;
-    public TextAsset TileSetXML;
 
-    private Dictionary<int, GameObject> m_level;
+    [SerializeField] private SpriteAtlas m_spriteAtlas;
+
+    private Dictionary<int, GameObject> m_tiles = new();
+    [SerializeField] private GameObject m_tilePrefab;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        BuildTileSet();
+
         // TODO: ANYTHING 30 make 19
         ParseLevelXML();
     }
@@ -22,7 +25,32 @@ public class LoadLevelFromXML : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void BuildTileSet()
+    {
+        Sprite[] sprites = new Sprite[m_spriteAtlas.spriteCount];
+
+        int amount = m_spriteAtlas.GetSprites(sprites);
+
+        BetterDebugging.Instance.Assert(amount > 0, "Texture atlas shouldn't be empty! Double check reference is assigned!");
+
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            GameObject newTile = new GameObject
+            {
+                name = sprites[i].name,
+                transform =
+                {
+                    parent = transform
+                }
+            };
+
+            newTile.AddComponent<SpriteRenderer>().sprite = sprites[i];
+
+            m_tiles.Add(i, newTile);
+        }
     }
 
     private void ParseLevelXML()
@@ -30,25 +58,29 @@ public class LoadLevelFromXML : MonoBehaviour
         XmlDocument xmlDoc = new XmlDocument();
         xmlDoc.LoadXml(LevelXML.text);
 
-        foreach(XmlNode node in xmlDoc.DocumentElement.ChildNodes)
+        foreach (XmlNode node in xmlDoc.DocumentElement.ChildNodes)
         {
             if (node.Name.Equals("layer"))
             {
                 XmlElement levelElem = (XmlElement)node;
-                int width = Int32.Parse(levelElem.GetAttributeNode("width").InnerXml);
-                int height = Int32.Parse(levelElem.GetAttributeNode("height").InnerXml);
+                int width = int.Parse(levelElem.GetAttributeNode("width").InnerXml);
+                int height = int.Parse(levelElem.GetAttributeNode("height").InnerXml);
 
-                String[] tiles = node.InnerText.Split(",");
+                string[] tiles = node.InnerText.Split(",");
 
-                for(int i = 0; i < width; i++)
+                for (int i = 0; i < height - 1; i++)
                 {
-                    for (int j = 0; j < height; j++)
+                    for (int j = 0; j < width - 1; j++)
                     {
-                        int ID = Int32.Parse(tiles[width * i + j]);
+                        BetterDebugging.Instance.Assert(width * i + j < tiles.Length, $"{i}  {j}   {width}    {width * i + j}");
+
+                        long ID = long.Parse(tiles[width * i + j]);
+
                         if (ID != 0)
                         {
                             BetterDebugging.Instance.DebugLog(ID.ToString(), BetterDebugging.eDebugLevel.Message);
                         }
+
                     }
                 }
             }
